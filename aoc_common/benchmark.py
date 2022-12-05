4a8dfe170fe6c204c9bd3f24dbb7892d98295fb5
mrkirby153 <mr.austinwhyte@gmail.com>
2022-12-04 22:04:18 -0800

    [msc] Fix benchmark

--- aoc_common/benchmark.py
+++ aoc_common/benchmark.py
@@ -8,9 +8,9 @@ def print_timing(func: callable) -> callable:
     """
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start = time.perf_counter()
+        start = time.perf_counter_ns()
         result = func(*args, **kwargs)
-        end = time.perf_counter()
-        print(f"{func.__name__} took {(end - start) * 1000:.8f} seconds")
+        end = time.perf_counter_ns()
+        print(f"{func.__name__} took {(end - start) / 1000000:0.1f} ms")
         return result
     return wrapper
