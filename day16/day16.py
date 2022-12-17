d8f8995f9ab6a3f7ea8b07ee47ded01b773592ce
mrkirby153 <mr.austinwhyte@gmail.com>
2022-12-16 17:02:08 -0800

    [Day 16] Part 2

--- day16/day16.py
+++ day16/day16.py
@@ -9,6 +9,7 @@ sys.path.append(root.as_posix())
 
 import re
 from functools import lru_cache
+from math import inf
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--sample', '-s', help='Run with sample data', action='store_true', default=False)
@@ -66,11 +67,56 @@ def get_max_flow_rate(current_position, opened_valves, time_left):
             best = max(best, get_max_flow_rate(adjacent, opened_valves, time_left - 1))
         return best
 
+valve_distances = {}
+
+def djikstra(valve):
+    possible = {valve: 0}
+    explored = set()
+    while len(explored) < len(valve_tunnels):
+        current = min(((k, v) for k, v in possible.items() if k not in explored), key=lambda x: x[1])[0]
+        for other in valve_tunnels[current]:
+            new_dist = possible[current] + 1
+            if possible.get(other, inf) > new_dist:
+                explored.discard(other)
+                possible[other] = new_dist
+            explored.add(current)
+    return possible
+
+
+valve_distances = {k: djikstra(k) for k in valve_tunnels.keys() }
+
+max_flow_seen = 0
+@lru_cache(maxsize=None)
+def run_part_2(cur, other, closed_valves):
+    cur_time_left, cur_pos = cur
+    other_time_left, other_pos = other
+    totals = [0]
+    for valve in closed_valves:
+        time_to_valve = valve_distances[cur_pos].get(valve) + 1
+        time_left = cur_time_left - time_to_valve
+        if time_left <= 0:
+            continue # Can't get to the valve and open it
+
+        flow_gained = time_left * valve_flow_rates[valve]
+
+        # Move the person that has the most time left
+        if time_left > other_time_left:
+            totals.append(flow_gained + run_part_2((time_left, valve), other, closed_valves - {valve}))
+        else:
+            totals.append(flow_gained + run_part_2(other, (time_left, valve), closed_valves - {valve}))
+    max_flow = max(totals)
+
+    global max_flow_seen
+    if max_flow > max_flow_seen:
+        print("New max:", max_flow)
+        max_flow_seen = max_flow
+    return max_flow
+
 def part_1():
     return get_max_flow_rate('AA', (), 30)
 
 def part_2():
-    pass
+    return run_part_2((26, 'AA'), (26, 'AA'), frozenset(potential_valves))
 
 print(f"Part 1: {part_1()}")
 print(f"Part 2: {part_2()}")
